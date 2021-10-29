7dd901e1ce93a898e5c1cc583588fb7af15cbe1e
bcod-dev <52274990+bcod-dev@users.noreply.github.com>
2021-10-29 19:04:40 +0530

    Console Index

--- web/public/_Scripts/main.js
+++ web/public/_Scripts/main.js
@@ -243,6 +243,7 @@ function doSubmit(){
 	 // Read selected files
 	var totalfiles = document.getElementById('image').files.length;
 	for (var index = 0; index < totalfiles; index++) {
+		console.log(index);
 		data.append("image[]", document.getElementById('image').files[index]);
 	}
     //data.append( 'image',  file), "filename";
