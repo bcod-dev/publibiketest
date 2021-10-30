b928d2a8bae2e90247ccd5838625ad70c9d339f6
bcod-dev <52274990+bcod-dev@users.noreply.github.com>
2021-10-30 19:15:00 +0530

    Add files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'cb6fe4f1b41d130bf2afe4afe15c0165efc10998',
+    'name' => 'jerry/tinyfw',
+  ),
+  'versions' => 
+  array (
+    'jerry/tinyfw' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'cb6fe4f1b41d130bf2afe4afe15c0165efc10998',
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.4.0',
+      'version' => '6.4.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '050d430203105c27c30efd1dce7aa421ad882d01',
+    ),
+  ),
+);
